6b79cfb11b36f80ccd1ec5d1d060a72ab11677a5
sim <simonas.b@hotmail.com>
2025-09-28 17:10:59 +0000

    add graceful shutdowns to http servers

--- services/trip-service/cmd/main.go
+++ services/trip-service/cmd/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
 	http_handlers "golang-ride-sharing/services/trip-service/internal/infrastructure/http"
 	"golang-ride-sharing/services/trip-service/internal/infrastructure/repository"
 	"golang-ride-sharing/services/trip-service/internal/service"
 	"golang-ride-sharing/shared/env"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 var (
@@ -32,8 +37,30 @@ func main() {
 		Handler: 	mux,
 	}
 
-	if err := server.ListenAndServe(); err != nil {
-		log.Printf("HTTP server error: %v", err)
+	serverErrors := make(chan error, 1)
+
+	go func() {
+		log.Printf("server listening on %s", httpAddr)
+		serverErrors <- server.ListenAndServe()
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case err:= <-serverErrors:
+		log.Printf("error starting the server: %v", err)
+
+	case sig := <-shutdown:
+		log.Printf("server is shutting down due to %v signal", sig)
+
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second * 10)
+		defer cancel()
+
+		if err := server.Shutdown(ctx); err != nil {
+			log.Printf("could not stop server gracefully: %v", err)
+			server.Close()
+		}
 	}
 
 }
